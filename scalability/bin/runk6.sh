f1c6cf783461cf91c25c7cdaa716ad0d3a695e9b
Evan Hughes <ehugh1@gmail.com>
2026-06-01 11:43:45 +1000

    temp

--- scalability/bin/runk6.sh
+++ scalability/bin/runk6.sh
@@ -10,10 +10,10 @@ mkdir -p logs
 
 # k6run 0_debug
 k6run 1_normal
-k6run 2_curiosity
-k6run 3_compliance_early
-k6run 4_compliance_mid
-k6run 5_compliance_peak
-k6run 6_compliance_tail
-k6run 7_auditing_bau
-k6run 8_research
+# k6run 2_curiosity
+# k6run 3_compliance_early
+# k6run 4_compliance_mid
+# k6run 5_compliance_peak
+# k6run 6_compliance_tail
+# k6run 7_auditing_bau
+# k6run 8_research
